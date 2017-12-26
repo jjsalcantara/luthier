5ab1f0009d25d85861566b788f5f1fa7994bdf87
jjsalcantara <jjsalcantara@gmail.com>
2017-12-26 18:53:22 -0300

    primeira versao

---
+++ LUTHIER/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LUTHIER.Startup))]
+namespace LUTHIER
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
